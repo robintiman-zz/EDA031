7609208355c9ca2a9fdd188e245c0ab5bb56cc98
maqwerton <dat13mjo@student.lu.se>
2017-02-21 10:30:53 +0100

    started lab4

---
+++ lab4/nameserver/VNS.cc
@@ -0,0 +1,40 @@
+#include "nameserverinterface.h"
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include <string>
+#include <tuple>
+
+using namespace std;
+using IPAddress = unsigned int;
+
+VNS::VNS(){
+vector<tuple<string, IPAddress>> v;
+}
+
+void insert(const HostName&, const IPAddress&) {
+  v.emblace_back(HostName&, IPAddress&)
+}
+
+bool remove(const HostName&, const IPAddress&)  {
+  v::iterator rem = find(v.begin(), v.end(), tuple<string, IPAddress>)
+  if (rem != v.end()) {
+    v.erase(rem);
+    return true;
+    }
+return false;
+}
+
+
+IPAddress lookup(const HostName&) {
+  auto lo = find_if(v.begin(), v.end(),
+  [&h](tuple<HostName, IPAddress> p) {return p[0] == h;});
+		if (lo != v.end()){
+			return lo->p[1];
+		}
+		return NON_EXISTING_ADDRESS;
+}
+
+int main()  {
+
+}
---
+++ lab4/nameserver/VNS.h
@@ -0,0 +1,15 @@
+
+
+#ifndef VNS_H
+#define NAME_SERVER_INTERFACE_H
+
+#include nameserverinterface.h
+#include <vector>
+
+
+class VNS {
+
+public:
+  virtual lookup
+
+}
