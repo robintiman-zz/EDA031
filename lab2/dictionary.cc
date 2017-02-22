ce309076b472c7893e7312d361e62e62eefecef9
Robin Timan <robintiman@gmail.com>
2017-02-22 16:27:40 +0100

    ..

--- lab2/dictionary.cc
+++ lab2/dictionary.cc
@@ -16,8 +16,8 @@ Dictionary::Dictionary() {
 	size_t pos_after_nbr;
 	while (getline(word_stream, line) && word_stream.is_open()) {
 		// Need trigrams and word from words.txt
-		pos = line.find_first_of(" ");
 		word_str = line.substr(0, pos);
+		pos = line.find_first_of(" ");
 		nbr_of_trigrams = stoi(line.substr(pos + 1, 2), &pos_after_nbr);
 		for (int i = 0; i < nbr_of_trigrams; ++i) {
 			trigrams.push_back(line.substr(pos_after_nbr + i * 3 + 1, 3));
@@ -45,10 +45,28 @@ vector<string> Dictionary::get_suggestions(const string& word) const {
 	return suggestions;
 }
 
-void Dictionary::add_trigram_suggestions(vector<string>& suggestions, const string& word) const{
+void Dictionary::add_trigram_suggestions(std::vector<std::string>& sug, const std::string& word) const {
 	int word_size = word.size();
+	vector<string> trigrams_of_word = calc_trigrams(word);
 	for (int i = word_size - 1; i <= word_size + 1; ++i) {
-		suggestions.insert(suggestions.end(), words[i].begin(), words[i].end());
+
+		sug.insert(sug.end(), words[i].begin(), words[i].end());
 	}
 
+}
+
+vector<string> Dictionary::calc_trigrams(const string& word) const {
+	vector<string> trigrams;
+	string trigram;
+	if (word.length() <= 3) {
+		trigrams.push_back(word);
+	} else {
+		for (unsigned i = 0; i < word.length() - 2; ++i) {
+			trigram = word.substr(i, 3);
+			transform(trigram.begin(), trigram.end(), trigram.begin(), ::tolower);
+			trigrams.push_back(trigram);
+		}
+		sort(trigrams.begin(), trigrams.end());
+	}
+	return trigrams;
 }
--- lab2/dictionary.h
+++ lab2/dictionary.h
@@ -15,10 +15,11 @@ private:
 	std::unordered_set<std::string> word_set;
 	const static int max_word_length = 25;
 	std::vector<Word> words [max_word_length];
-	void add_trigram_suggestions(std::vector<std::string>& suggestions, const std::string& word) const;
+	void add_trigram_suggestions(std::vector<std::string>& sug, const std::string& w) const;
 	void rank_suggestions(std::vector<std::string>& suggestions, const std::string& word) const;
 	std::vector<std::string> word_to_trigrams(const std::string s) const;
 	void trim_suggestions(std::vector<std::string>& suggestions) const;
+	std::vector<std::string> calc_trigrams(const std::string& word) const;
 };
 
 #endif
