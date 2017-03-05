a75c6e887f7b231fbeecaf6b5673b7fe6984abca
Robin Timan <robintiman@gmail.com>
2017-03-05 11:06:06 +0100

    askdljgnsw

--- lab2/word.cc
+++ lab2/word.cc
@@ -13,10 +13,10 @@ string Word::get_word() const { return word; }
 
 unsigned int Word::get_matches(const vector<string> &t) const {
   int matches = 0;
-  int param_count, local_count = 0;
+  size_t param_count = 0, local_count = 0;
   while (param_count != t.size() && local_count != trigrams.size()) {
-    string local_t = trigrams.at(i);
-    string param_t = t.at(j);
+    string local_t = trigrams.at(local_count);
+    string param_t = t.at(param_count);
     if (local_t == param_t) {
       ++matches;
       ++local_count;
